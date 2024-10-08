6e8fa6d9486bfc9ad5d4b39a903daf796baaee88
BradenEverson <bradeneverson@gmail.com>
2024-10-07 19:04:01 -0500

    Add expects instead of gross box dyn error

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,20 @@
 use rppal::gpio::Gpio;
 use rppal::uart::{Parity, Uart};
-use std::error::Error;
 use std::time::Duration;
 
 const MOTOR_CONTROL_PIN: u8 = 18;
-fn main() -> Result<(), Box<dyn Error>> {
-    let gpio = Gpio::new()?;
-    let mut motor_pin = gpio.get(MOTOR_CONTROL_PIN)?.into_output();
+fn main() {
+    let gpio = Gpio::new().expect("Failed to setup GPIO");
+    let mut motor_pin = gpio.get(MOTOR_CONTROL_PIN).expect("Failed to establish motor pin").into_output();
 
     motor_pin.set_high();
 
-    let mut uart = Uart::new(115_200, Parity::None, 8, 1)?;
+    let mut uart = Uart::new(115_200, Parity::None, 8, 1).expect("Failed to set UART channel");
 
-    uart.set_read_mode(255, Duration::from_millis(1000))?;
+    uart.set_read_mode(255, Duration::from_millis(1000)).expect("Failed to set read mode");
 
     let start_scan_cmd: [u8; 2] = [0xA5, 0x20];
-    uart.write(&start_scan_cmd)?;
+    uart.write(&start_scan_cmd).expect("Failed to send start cmd");
 
     println!("RPLidar A1 scanning...");
 
