bee8ad2438927b1846fddf108f2129fa4f22c772
BradenEverson <bradeneverson@gmail.com>
2024-10-07 19:19:33 -0500

    Use packet format

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use rppal::uart::{Parity, Uart};
 use std::time::Duration;
 
 const MOTOR_CONTROL_PIN: u8 = 18;
+
 fn main() {
     let gpio = Gpio::new().expect("Failed to setup GPIO");
     let mut motor_pin = gpio.get(MOTOR_CONTROL_PIN).expect("Failed to establish motor pin").into_output();
@@ -11,25 +12,28 @@ fn main() {
 
     let mut uart = Uart::new(115_200, Parity::None, 8, 1).expect("Failed to set UART channel");
 
-    uart.set_read_mode(255, Duration::from_millis(1000)).expect("Failed to set read mode");
+    uart.set_read_mode(16, Duration::from_secs(1)).expect("Failed to set read mode");
 
     let start_scan_cmd: [u8; 2] = [0xA5, 0x20];
     uart.write(&start_scan_cmd).expect("Failed to send start cmd");
 
     std::thread::sleep(Duration::from_millis(100));
-
     println!("RPLidar A1 scanning...");
 
-    let mut buffer: Vec<u8> = vec![0; 512];
+    let mut buffer: Vec<u8> = vec![0; 16];
 
     loop {
         match uart.read(&mut buffer) {
             Ok(n) if n > 0 => {
-                println!("Received {} bytes: {:X?}", n, &buffer[..n]);
+                println!("Received {} bytes: {:02X?}", n, &buffer[..n]);
+                
+                if n == 5 && buffer[0] == 0xA5 && buffer[1] == 0x5A {
+                    println!("Received RPLidar response header.");
+                }
             }
             Ok(_) => {
                 println!("No data received, retrying...");
-                std::thread::sleep(Duration::from_millis(500));  // Retry after a short delay.
+                std::thread::sleep(Duration::from_millis(100));
             }
             Err(e) => {
                 eprintln!("Failed to read from UART: {}", e);
